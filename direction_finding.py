7d792c68abaa09325fca34080cb729dbec4f053a
anhtuan <a.t.nguyen@student.tudelft.nl>
2024-11-20 12:05:21 +0100

    alles

--- direction_finding.py
+++ direction_finding.py
@@ -1,3 +1,14 @@
-testtest 
-test2test2
-hallo
+import matplotlib.pyplot as plt           # For plotting purposes
+import numpy as np                        # For convolution function
+import scipy
+from scipy import signal                  # For filter function
+from scipy.fft import fft, ifft           # For fft and ifft
+import math                               # For numerical approximation of pi
+
+#IP3 direction M3 M4 part 
+#M is the number of microphones
+# d is the distance between the microphones
+#v is the speed of sound
+# f0 is the center frequency of the wave in hertz
+
+def a_lin(theta, M, d,v, f0):
