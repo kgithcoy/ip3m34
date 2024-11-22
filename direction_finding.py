37f669d136d0f8f72a3f52967a6309c928e054aa
anhtuan <a.t.nguyen@student.tudelft.nl>
2024-11-22 11:29:03 +0100

    assignment 6.2.3 done/tested

--- direction_finding.py
+++ direction_finding.py
@@ -1,26 +1,26 @@
-import matplotlib.pyplot as plt           # For plotting purposes
-import numpy as np                        # For convolution function
-import scipy
-from scipy import signal                  # For filter function
-from scipy.fft import fft, ifft           # For fft and ifft
-import math                               # For numerical approximation of pi
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.fft import fft, ifft
+import math
 
-#IP3 direction M3 M4 part 
-#M is the number of microphones
-# d is the distance between the microphones
-#v is the speed of sound
-# f0 is the center frequency of the wave in hertz
+# IP3 direction M3 M4 part
+# M: Number of microphones
+# d: Distance between microphones
+# v: Speed of sound
+# f0: Center frequency of the wave in hertz
 
 def a_lin(theta, M, d, v, f0):
-    omega = f0/(math.pi)
-    t = (d/v)*math.sin(theta)
-    entries = np.linspace(0, M-1, M)
-    
-    vector = np.exp(-1j*omega*t*entries)
-    s = np.exp(1j*omega*t)
-    
+    omega = 2 * math.pi * f0
+    phase_delay = (d / v) * np.sin(theta)
+    entries = np.arange(M)  
+    vector = np.exp(-1j * omega * phase_delay * entries)
     return vector
 
-result = a_lin(0, 4, 0.1, 343, 1000)
+theta = 50  
+M = 4      
+d = 2    
+v = 340  
+f0 = 1000  
+
+result = a_lin(theta, M, d, v, f0)
 print(result)
-print("1")
