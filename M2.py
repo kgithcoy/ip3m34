d2f87ccff1d2a03286e742c22554e6c416053de2
Tony <zehangliu077@gmail.com>
2024-12-01 23:32:21 +0100

    Module 2 done

--- M2.py
+++ M2.py
@@ -10,20 +10,29 @@ from scipy import signal
 from scipy.signal import butter, filtfilt
 from scipy.signal import convolve, unit_impulse, freqz, spectrogram
 from scipy.signal.windows import gaussian
+from scipy.signal import find_peaks, peak_prominences, peak_widths
+from scipy.signal import stft, TransferFunction, lsim
 
 # Import own made utilies:
 from Tony import *
 
+
 # Import audiorecording
-audio_sample_rate, data = wavfile.read("heart_single_channel_physionet_49829_TV.wav")
+#audio_sample_rate, data= wavfile.read("50763_AV kopie.wav")
+#audio_sample_rate, data= wavfile.read("heart_single_channel_physionet_49829_TV kopie.wav")
+audio_sample_rate, data = wavfile.read("recording_heart_ownDevice kopie.wav")
 data = data.astype(float)
-#data = normalize(data)
+#data = normalize(data) # If needed, normalize the data
 
 # Display details
 print(f"Audio Sample Rate: {audio_sample_rate} Hz")
 print(f"Number of Samples: {len(data)}")
 print(f"Shape of the recording: {data.shape}")
 
+#---------------------------------------------------------------------------------------------------------------------------------
+# M1:
+
+# Parameters:
 Fs_new = 3998 #[Hz]
 cutoff_low = 20 # Filtering out DC-components
 cutoff_high = 200
@@ -43,17 +52,61 @@ downsampled_data_normalize = normalize(downsampled_data) #Normalizig the downsam
 shannon_signal = shannon_energy(downsampled_data_normalize)
 SEE = zero_phase_butter_filter(shannon_signal, Fs_new, order, 15, "low") # Apply lowpass-(zero-phase)filtering at 15Hz
 SEE = normalize(SEE)
+# Segmentation
+#segmentation(SEE, Fs_new, 0.4, verbose=False)
 
 # Plots:
-spectogram(downsampled_data_normalize, Fs_new, 0, 0)
-wavfile_plot(shannon_signal, Fs_new, "Shannon Energy",[[0,10],0],0)
-wavfile_plot(SEE, Fs_new, "SEE", [[2,8],0], 0)
-wavfile_plot(filtered_data, Fs_new, "Band Filterd signal 2kHz of heart_single_channel_physionet_49829_TV", 0, 0)
-wavfile_plot_combined(shannon_signal, SEE, Fs_new, "Shannon energy", "SEE")
-wavfile_plot(downsampled_data, Fs_new, "Downsampled heart_single_channel_physionet_49829_TV",0,0)
+#spectogram(downsampled_data_normalize, Fs_new, 0, 0)
+#wavfile_plot(shannon_signal, Fs_new, "Shannon Energy",[[0,10],0],0)
+#wavfile_plot(SEE, Fs_new, "SEE", [[2,8],0], 0)
+#wavfile_plot(filtered_data, Fs_new, "Band Filterd signal 2kHz of heart_single_channel_physionet_49829_TV", 0, 0)
+#wavfile_plot_combined(shannon_signal, SEE, Fs_new, "Shannon energy", "SEE")
+#wavfile_plot(downsampled_data, Fs_new, "Downsampled heart_single_channel_physionet_49829_TV",0,0)
 #wavfile_plot(data, audio_sample_rate, "heart_single_channel_physionet_49829_TV",0,0)
 #wavfile_plot(downsampled_data_normalize, Fs_new, "Downsampled Normalized heart_single_channel_physionet_49829_TV",0,0)
 #wavfile_plot_combined(downsampled_data, downsampled_data_normalize, Fs_new, "Downsampled unnormalized", "Downsampled normalized")
+
+#---------------------------------------------------------------------------------------------------------------------------------
+# M2:
+
+# Parameters:
+Fs = 4000
+T_total = 1 # in seconds
+v_sound = 60  # Speed of sound in the body in m/s
+repetitions = 10 # Repete the heartbeats response this many time
+
+# Computing all valves response M, T, A, P (with or without noise)
+M_t, M = pole_pairs(20, 50, 1, 10, Fs, T_total, 10, addnosie=False)
+T_t, T = pole_pairs(20, 150, 0.5, 40, Fs, T_total, 10, addnosie=False)
+A_t, A = pole_pairs(20, 50, 0.5, 300, Fs, T_total, 10, addnosie=False)
+P_t, P = pole_pairs(20, 30, 0.4, 330, Fs, T_total, 10, addnosie=False)
+# System of multichannel:
+# Valve, location of [x,y,z] in cm!
+valve = np.array([[6.37, 10.65, 6.00],     # Valve[0] = M , The assigned position index of the valve name is important
+                    [0.94, 9.57, 5.50],    # Valve[1] = T
+                    [5.50, 11.00, 3.60],   # Valve[2] = A
+                    [3.90, 11.50, 4.50]])  # Valve[3] = P
+
+# Microphone, location of [x,y,z] in cm!
+microphone = np.array([[2.5, 5.0, 0],    # Mic[0]
+                        [2.5, 10.0, 0],  # Mic[1]
+                        [2.5, 15.0, 0],  # Mic[2]
+                        [7.5, 5.0, 0],   # Mic[3] 
+                        [7.5, 10.0, 0],  # Mic[4]
+                        [7.5, 15.0, 0]]) # Mic[5]
+
+# Compute the unfiltered multichannel, if needed we can plot it all in one figure
+microphone_multichannel_unfiltered, distance, attenuation, delay = multi_channel_signal(M, T, A, P, Fs, valve, microphone, plotting=True)
+
+# Display details
+print(f"Micrphone output:{microphone_multichannel_unfiltered}")
+print(f"Distance:{distance}")
+print(f"Attenuation:{attenuation}")
+print(f"Delay:{delay}")
+
+# Plots:
+heartbeat_plot(M ,T, A, P, M_t, T_t, A_t ,P_t, Fs, T_total, repetitions=repetitions)
+
 
 
 
--- Tony.py
+++ Tony.py
@@ -4,23 +4,25 @@ from scipy.io import wavfile        #For using .wav type datas
 from IPython.display import Audio   #For audio display if needed
 
 # Scipy modules
-from scipy.signal import stft, TransferFunction, lsim
+from scipy.signal import stft
 from scipy import signal
 #from scipy.fft import fft, ifft    #If we have this, then np.fft.abcd in codes can be changed to abcd
 from scipy.signal import butter, filtfilt
 from scipy.signal import convolve, unit_impulse, freqz, spectrogram
 from scipy.signal.windows import gaussian
+from scipy.signal import find_peaks, peak_prominences, peak_widths
+from scipy.signal import stft, TransferFunction, lsim
 
 def wavfile_plot(x,Fs,name,tinterest,freqinterest):
     """
     Plots the time-domain and frequency-domain representation of an audio signal.
 
     Parameters:
-    - x: The audio data (numpy array).
+    - x: The audio data (array-like).
     - Fs: Sampling frequency in Hz (float).
     - name: Name of the signal (string).
-    - tinterest: time-domain  x-axis(time) and y-axis(amplitude) limiting zoom in ([[xlim],[ylim]]). 0 for whole axis
-    - freqinterest: frequency-domain x-axis(frequencu) and y-axis(magnitude) limiting zoom in ([[xlim],[ylim]]). 0 for whole axis
+    - tinterest: time-domain  x-axis(time) and y-axis(amplitude) limiting zoom in ([[xlim],[ylim]]). 0 for whole axis.
+    - freqinterest: frequency-domain x-axis(frequencu) and y-axis(magnitude) limiting zoom in ([[xlim],[ylim]]). 0 for whole axis.
     """
     t = np.linspace(0, len(x)/Fs, len(x))
     f = np.fft.fftfreq(len(x), d=1/Fs)
@@ -46,15 +48,15 @@ def wavfile_plot(x,Fs,name,tinterest,freqinterest):
     fig.tight_layout()
     plt.show()
 
-def response_plot(x,Fs,name,logfreq):
+def response_plot(x,Fs,name,logfreq=False):
     """
-    Plots the time-domain and frequency-domain representation of an response signal
+    Plots the time-domain and frequency-domain representation of an response signal.
 
     Parameters:
-    - x: The response data (numpy array).
+    - x: The response data (array-like).
     - Fs: Sampling frequency in Hz (float).
     - name: Name of the signal (string).
-    - logfreq: Boolean indication if logarithic x-axis is whished. (binary)
+    - logfreq: Boolean indication if logarithic x-axis is whished, default=False (Boolean).
     """
     t = np.linspace(0, len(x)/Fs, len(x))
     f = np.linspace(0, Fs, len(x))
@@ -72,30 +74,29 @@ def response_plot(x,Fs,name,logfreq):
     ax[1].set_title(f"Frequency domain: {name}")
     ax[1].set_ylabel("Magnitude [dB]")
     ax[1].set_xlabel("Frequency [Hz]")
-    if logfreq == 1:
-        ax[1].set_xscale("log")
     ax[1].grid()
     ax[2].plot(f, X_phase)
     ax[2].set_title(f"Frequency domain: {name}")
     ax[2].set_ylabel("Phase [rad]")
     ax[2].set_xlabel("Frequency [Hz]")
-    if logfreq == 1:
-        ax[2].set_xscale("log")
     ax[2].grid()
+    if logfreq:
+        ax[1].set_xscale("log")
+        ax[2].set_xscale("log")
     fig.tight_layout()
     plt.show()
 
-def give_M(Fs_new, Fs_old):
-    M = Fs_old // Fs_new # Downsampling factor
+def give_M(Fs_new, Fs_old):     # Fs_old is the original sampling frequency and Fs_new is the sampling frequency you wanted.
+    M = Fs_old // Fs_new        # Compute the Downsampling factor and return it
     return M
 
 def wavfile_plot_combined(x1, x2, Fs, name1, name2):
     """
-    Plots the time-domain and frequency-domain representation of two audio signals in one figure.
+    Plots the time-domain representation of two audio signals in one figure and plots the the frequency-domain representation independently.
 
     Parameters:
-    - x1: The first audio data (numpy array).
-    - x2: The second audio data (numpy array).
+    - x1: The first audio data (array-like).
+    - x2: The second audio data (array-like).
     - Fs: Sampling frequency in Hz (float).
     - name1: Name of the first signal (string).
     - name2: Name of the second signal (string).
@@ -135,15 +136,15 @@ def wavfile_plot_combined(x1, x2, Fs, name1, name2):
 
 def downsampling(x, Fs_x, Fs_new):
     """
-    Downsample a signal to a new sampling rate 
+    Downsample a signal to a new sampling rate .
 
     Parameters:
-    - x (array-like): Input signal to be downsampled.
-    - Fs_x (float): Original sampling rate of the signal.
-    - Fs_new (float): Desired new sampling rate.
+    - x: Input signal to be downsampled (array-like).
+    - Fs_x: Original sampling rate of the signal in Hz (float).
+    - Fs_new: Desired new sampling rate in Hz ((float)).
 
     Returns:
-    - downsampled_signal (array-like): Signal downsampled to the new sampling rate.
+    - downsampled_signal: Signal downsampled to the new sampling rate (array-like).
     """
     if Fs_new >= Fs_x:
         raise ValueError(f"New sampling rate ({Fs_new}) must be less than the original rate ({Fs_x}).")
@@ -151,11 +152,20 @@ def downsampling(x, Fs_x, Fs_new):
     downsampled_signal = x[::M]
     return downsampled_signal
 
-def normalize(x):
+def normalize(x): # Funtion to normalize a list/np.array x.
     x = x / np.max(np.abs(x))
     return x
 
 def shannon_energy(x):
+    """
+    Compute the shannon-energy of a signal.
+
+    Parameters:
+    - x: Input signal (array-like).
+
+    Returns:
+    - E_s: The shannon-energy of a signal (array-like).
+    """
     x = np.abs(x)
     x_squared = x**2
     E_s = ( -x_squared ) * np.log10(np.maximum(x_squared,1e-15))
@@ -166,14 +176,14 @@ def zero_phase_butter_filter(x, Fs, order, cutoff, filter_type):
     Zero-phase Butterworth filtering with filtfilt.
 
     Parameters:
-    - x: Input signal (numpy array).
+    - x: Input signal (array-like).
     - Fs: Sampling frequency in Hz (float).
     - order: Filter order (int).
     - cutoff: Cutoff frequency in Hz (float) for lowpass/highpass or list of two floats for bandpass/bandstop.
     - filter_type: Type of filter ('low', 'high', 'band', 'stop').
 
     Returns:
-    - filtered_x: Filtered signal (numpy array).
+    - filtered_x: Filtered signal (array-like).
     """
     if isinstance(cutoff, (list, tuple)) and len(cutoff) == 2:
         # Bandpass or bandstop filter
@@ -198,12 +208,12 @@ def check_attenuation(b, a, Fs, F_interest, worn=8000):
 
     Parameters:
     - b, a: Filter coefficients (numerator and denominator).
-    - Fs: Sampling frequency in Hz.
-    - F_interest: Frequency of interest to check the attenuation (in Hz).
+    - Fs: Sampling frequency in Hz (float).
+    - F_interest: Frequency of interest to check the attenuation in Hz (float).
     - worn: Number of frequency points to compute the frequency response (default=8000).
 
     Returns:
-    - dB_value: The dB attenuation at the frequency of interest.
+    - dB_value: The dB attenuation at the frequency of interest (float).
     """
     w, h = freqz(b, a, worN=worn)   # Compute frequency response
     f = (Fs / 2) * w  / np.pi       # Convert frequency axis to Hz depending on the sampling rate Fs (y-axis)
@@ -218,15 +228,10 @@ def spectogram(x, Fs, t_interest, freqinterest, nperseg=256, noverlap=128):
     Plot the spectogram of a given signal.
 
     Parameters:
-    - x: Input of the analysed signal (numpy array).
+    - x: Input of the analysed signal (array-like).
     - Fs: Sampling rate of the signal in Hz (float).
     - t_interest: The time-interval of interest (array of two indices), put 0 if want whole axis.
     - freqinterest: The frequency-interval of interest, note is double-sided, put 0 if want whole axis.
-
-
-
-
-
     """
     window = gaussian(nperseg, std=1e-2 * Fs) # Make a Gaussian windo
     freq, times, Sxx = spectrogram(           # Compute the spectogram variables
@@ -260,14 +265,31 @@ def spectogram(x, Fs, t_interest, freqinterest, nperseg=256, noverlap=128):
         plt.ylim(freqinterest)
     plt.show()
     
-def pole_pairs(duration_ms, freq_hz, amplitude, onset_delay_ms, Fs, T_total):
 
+def pole_pairs(duration_ms, freq_hz, amplitude, onset_delay_ms, Fs, T_total, Ns, addnosie=False):
+    
+    """
+    Simulates the impulse response of a second-order system defined by the given parameters or in other words: generation of the valve-response.
+
+    Parameters:
+    - duration_ms: Duration of the impulse signal in milliseconds (float).
+    - freq_hz: Natural frequency of the system in Hz (float).
+    - amplitude: Amplitude scaling factor for the system's output (float).
+    - onset_delay_ms: Delay before the impulse signal starts, in milliseconds (float).
+    - Fs: Sampling frequency in Hz (float).
+    - T_total: Total duration of the simulation in seconds (float).
+    - Nrep: Number of non-zero samples to include in the random noise if `addnoise` is True (int).
+    - addnoise: If True, adds random noise to the impulse signal; otherwise, a single impulse is use, default=False (Boolean).
+
+    Returns:
+        t_out: Time vector corresponding to the system's response (array-like).
+        h_out: Scaled impulse response of the system (array-like).
+    """
     t = np.linspace(0, T_total, int(Fs * T_total))
     duration = duration_ms / 1000
     onset_delay = onset_delay_ms / 1000
     omega_0 = 2 * np.pi * freq_hz
     a = 1 / duration
-
 
     num = [omega_0]
     den = [1, 2 * a, a ** 2 + omega_0 ** 2]
@@ -275,17 +297,238 @@ def pole_pairs(duration_ms, freq_hz, amplitude, onset_delay_ms, Fs, T_total):
     system = TransferFunction(num, den)
 
     impulse = np.zeros_like(t)
-    impulse[int(Fs * onset_delay)] = 1
+    if not addnosie:
+        impulse[int(Fs * onset_delay)] = 1
 
-    # N = int(Fs * duration)
-    # Ns = 10
-    # s = np.concatenate((np.random.randn(Ns), np.zeros(N - Ns)))
-    # impulse[int(Fs * onset_delay):int(Fs * onset_delay) + N] = s
+    N = int(Fs * duration)
+    if addnosie:
+        s = np.concatenate((np.random.randn(Ns), np.zeros(N - Ns)))
+        impulse[int(Fs * onset_delay):int(Fs * onset_delay) + N] = s
     
-
     t_out, h_out, _ = lsim(system, U=impulse, T=t)
 
     h_out = amplitude * h_out
 
     return t_out, h_out
-    
+
+def segmentation(x, Fs, peak_find_boundary, wlen=800, verbose=False):
+    """
+    Segmentate the heart beat respones and indicate the S1 & S2 peaks in a plot.
+
+    Parameters:
+    x: Input of a 1D signal to segmentate (array-like).
+    Fs: The sampling frequency of the signal (float).
+    peak_find_boundary: The boundary of the amplitude of the threshold to find a peak (Threshold between:(a,b) or threshold under a) (float).
+    wlen: Window lenth in samples for analyzing the promineces, default=800 (int).
+    verbose: Printing the important values when True, default=False (Boolean).
+    """
+    t = np.linspace(0, len(x) /Fs, len(x)) # Define the time axis for the data that you want to segment
+
+    peaks_index, _ = find_peaks(x, peak_find_boundary) 
+    peaks_index_t = peaks_index / Fs # From samples index to time index
+    prominences,leftb,rightb = peak_prominences(x, peaks_index, wlen=wlen)
+    pp_height = x[peaks_index] - prominences # The height difference between the prominence and the peak
+    width = np.diff(peaks_index) # This computes the difference between the peaks in amount of samples, note always len(width) = len(peaks_index) - 1
+
+    S1 = []
+    S2 = []
+    for i in range(len(width)-1):
+        if width[i] > width[i+1]:
+            S2 = np.append(S2, peaks_index[i])
+            S1 = np.append(S1, peaks_index[i-1])
+        else:
+            S2 = np.append(S2, peaks_index[i+1])
+            S1 = np.append(S1, peaks_index[i])
+        
+    S1 = np.unique(S1).astype(int)
+    S2 = np.unique(S2).astype(int)
+
+    if verbose:
+        print(f"Prominences value:{prominences}")
+        print(f"Peak to prominence difference:{x[peaks_index]-prominences}")
+        print(f"Samples of peaks measured:{len(peaks_index)}")
+        print(f"Samples of width measured:{len(width)}")
+        print(f"Peaks_index given (in samples):{peaks_index}")
+        print(f"Peaks_index of S1:{S1}")
+        print(f"Peaks_index of S2:{S2}")
+
+
+    fig , ax = plt.subplots(4,1, figsize=(6, 10))
+    ax[0].plot(t, x)
+    ax[0].set_title("Signal response")
+    ax[0].set_xlabel("Time [s]")
+    ax[0].set_ylabel("Amplitude")
+    ax[0].plot(peaks_index_t, x[peaks_index], "x", label="Peaks")
+    #ax[0].plot(peaks_index_t, prominences, "o", color="g")
+    ax[0].plot(leftb / Fs, x[leftb], "o", color="b", label="Left-peak bases")
+    ax[0].plot(rightb / Fs, x[rightb], "o", color="r", label="Right-peak bases")
+    ax[0].scatter(S1/ Fs, x[S1],marker="d", c="y" ,label= "S1" )
+    ax[0].scatter(S2/ Fs, x[S2],marker="d", c="k" ,label= "S2")
+    ax[0].grid()
+    ax[0].legend(fontsize=6)
+    ax[1].stem(prominences)
+    ax[1].set_title("Prominences of each peak")
+    ax[1].set_xlabel("Sample index n")
+    ax[1].set_ylabel("Amplitude")
+    ax[2].stem(pp_height)
+    ax[2].set_title("Peak to prominence height")
+    ax[2].set_xlabel("Sample index n")
+    ax[2].set_ylabel(" Amplitude height(difference)")
+    ax[3].stem(width)
+    ax[3].set_title("Width between peaks")
+    ax[3].set_xlabel("Sample index n")
+    ax[3].set_ylabel("Width in samples n")
+    plt.tight_layout()
+    plt.show()
+
+def heartbeat_plot(M ,T, A, P, M_t, T_t, A_t, P_t, Fs, T_total, repetitions=1):
+    """
+    Plots the individual responses of valves and the repeated combined impulse response.
+
+    Parameters:
+    - M: Impulse response of valve M (array-like).
+    - T: Impulse response of valve T (array-like).
+    - A: Impulse response of valve A (array-like).
+    - P: Impulse response of valve P (array-like).
+    - Fs: Sampling frequency in Hz (float).
+    - T_total: Total duration of one combined response in seconds (float).
+    - repetitions: Number of times to repeat the combined response in the plot, default=1 (int).
+    """
+    if not (len(M) == len(T) == len(A) == len(P)):
+        raise ValueError("The sizes of M, T, A, and P must be the same.") # Note that the size of all valves must be the same: 1D-array.
+    combined_response = M + T + A + P
+    combined_response_normalize = normalize(combined_response)
+    repeated_response = np.tile(combined_response_normalize, repetitions)
+
+    fig, ax = plt.subplots(4, 1, figsize=(6,10))
+    ax[0].plot(M_t, M)
+    ax[0].set_title("Valve M")
+    ax[0].set_xlabel("Time [s]")
+    ax[0].set_ylabel("Amplitude")
+    ax[1].plot(T_t, T)
+    ax[1].set_title("Valve T")
+    ax[1].set_xlabel("Time [s]")
+    ax[1].set_ylabel("Amplitude")
+    ax[2].plot(A_t, A)
+    ax[2].set_title("Valve A")
+    ax[2].set_xlabel("Time [s]")
+    ax[2].set_ylabel("Amplitude")
+    ax[3].plot(P_t, P)
+    ax[3].set_title("Valve P")
+    ax[3].set_xlabel("Time [s]")
+    ax[3].set_ylabel("Amplitude")
+    plt.tight_layout()
+    plt.show()
+
+    plt.plot(np.linspace(0, T_total * repetitions, int(Fs * T_total * repetitions)), repeated_response)
+    plt.title(f'Repeated Impulse Response of All Valves {repetitions} times')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude (Normalized)')
+    plt.show()
+
+def propagation(x, G, delay, Fs):
+    """
+    Simulates the propagation of a signal with attenuation and delay.
+
+    Parameters:
+    - x: Input signal to be propagated (array-like or scalar).
+    - G: Attenuation factor (float).
+    - delay: Delay in seconds (float).
+    - Fs: Sampling frequency in Hz (float).
+
+    Returns:
+    - x: Propagated signal with attenuation and delay applied (array-like).
+    """
+    if np.isscalar(x):
+        x = np.array([x])
+    x = np.pad(x,(int(delay*Fs),0))
+    x = x * G
+    return x
+
+def attenuationANDdelay(z_v, z_m):
+    """
+    Calculates the attenuation and delay between two points based on their positions.
+
+    Parameters:
+    - z_v: Position of the source(valve) [x, y, z](array-like).
+    - z_m: Position of the receiver(microphone) [x, y, z](array-like).
+
+    Returns:
+    - G: Attenuation factor (1 / distance) (float).
+    - delay: Propagation delay in seconds (float).
+    - d: Distance between the source and receiver (float).
+
+    Notes:
+    - The input z_v, z_m must in cm, as it is designed to calculate the output in small dimensions (human body).
+
+    """
+    z_v = z_v / 100 # Convert cm to m
+    z_m = z_m /100  # Convert cm to m
+    v = 60 # The velocity of sound in a body
+    d = np.linalg.norm(z_v - z_m)
+    G = 1 / d
+    delay = (d / v)
+    return G, delay, d
+
+def multi_channel_signal(M, T, A, P, Fs, valves, microphones, verbose=False, plotting=False):
+    """
+    Simulates multi-channel signals from multiple valves to multiple microphones.
+
+    Parameters:
+    - M, T, A, P (array-like): Signals response from valves M, T, A, and P.
+    - Fs (float): Sampling frequency in Hz.
+    - valves (numpy.ndarray): 2D array representing positions of valves in cm.
+    - microphones (numpy.ndarray): 2D array representing positions of microphones in cm.
+    - verbose (bool, optional): If True, prints details for each valve-to-microphone propagation parameters.
+    - plotting (bool, optional): If True, plots the resulting signals received for each microphone in one graph.
+
+    Returns:
+    - mic_signal (list of numpy.ndarray): Signals received at each microphone.
+    - distance (numpy.ndarray): Distance matrix between valves and microphones.
+    - G (numpy.ndarray): Attenuation matrix between valves and microphones.
+    - delay (numpy.ndarray): Delay matrix between valves and microphones.
+    """
+
+    distance = np.empty((microphones.shape[0],valves.shape[0]))
+    G = np.empty((microphones.shape[0],valves.shape[0]))
+    delay = np.empty((microphones.shape[0],valves.shape[0]))
+    mic_signal =[[] for _ in range(microphones.shape[0])]
+
+    for i in range(microphones.shape[0]):
+        for j in range(valves.shape[0]):
+
+            G_value, delay_value, distance_value = attenuationANDdelay(valves[j], microphones[i])
+            if verbose:
+                print(f"valve[{j}] -- mic[{i}]:\n Attenuation:{G_value:.3f}, Delay:{(delay_value * 1000):.5f} ms, Distance:{(distance_value * 100):.2f} cm\n")
+            G[i,j] = G_value
+            delay[i, j] = delay_value
+            distance[i, j] = distance_value
+
+    for i, (G_r, delay_r) in enumerate(zip(G,delay)):
+        M_pro = propagation(M, G_r[0], delay_r[0], Fs)
+        T_pro = propagation(T, G_r[1], delay_r[1], Fs)
+        A_pro = propagation(A, G_r[2], delay_r[2], Fs)
+        P_pro = propagation(P, G_r[3], delay_r[3], Fs) 
+        max_index = max(len(M_pro), len(T_pro), len(A_pro), len(P_pro))
+        M_pro = np.pad(M_pro, (0,max_index - len(M_pro)))
+        T_pro = np.pad(T_pro, (0,max_index - len(T_pro)))
+        A_pro = np.pad(A_pro, (0,max_index - len(A_pro)))
+        P_pro = np.pad(P_pro, (0,max_index - len(P_pro)))
+        mic_signal[i] = M_pro + T_pro + A_pro + P_pro
+        if verbose:
+            print(f"The length at microphone channel {i} is {len(mic_signal[i])} samples")
+
+    if plotting:
+        plt.figure(figsize=(10, 6))  # Set figure size
+        for i, row in enumerate(mic_signal):
+            plt.plot(row, label=f"Signal {i+1}")  # Plot with a unique label for each row
+        # Add legend, title, and grid
+        plt.legend()
+        plt.title("All Mic Signals")
+        plt.xlabel("Sample Index")
+        plt.ylabel("Amplitude")
+        plt.grid()
+        # Show the plot
+        plt.show()
+
+    return mic_signal, distance, G, delay 
